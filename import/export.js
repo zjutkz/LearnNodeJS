2cf3a8d32ee992a2ebbc34b4b3d2ad050431f04a
亢哲 <kangzhe@weidian.com>
2018-02-27 14:49:37 +0800

    add ryf's blog link

--- import/export.js
+++ import/export.js
@@ -1,4 +1,5 @@
 //module.export.xx相当于多了一个别名，在require的时候可以用这个别名
+//http://www.ruanyifeng.com/blog/2015/05/require.html
 
 var User = function(name, email) {
     console.log("name: " + name + " & email: " + email);
