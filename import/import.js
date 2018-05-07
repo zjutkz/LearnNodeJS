e37245ab96197f8f4c95f78d85547aa1b23d2845
kangzhe <kangzhe@weidian.com>
2018-05-07 17:34:25 +0800

    import

--- import/import.js
+++ import/import.js
@@ -1,10 +1,11 @@
-var user = require("./export");
+// var user = require("./export");
 user.logUser("zjurkz","suuuperwayne@gmail.com");
-//user("zjurkz","suuuperwayne@gmail.com");
+user("zjurkz","suuuperwayne@gmail.com");
 
 //==============================================================
 
 //require的时候可以直接在后面加上参数
 
 // var user = require("./export")(9050);
-// console.log(user(9050));
+// var user = require("./export")
+// console.log(user(8000));
