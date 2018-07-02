9d04f1c7ba2fdfebc1799356ca238824971d25e8
kangzhe <kangzhe@weidian.com>
2018-07-02 22:02:19 +0800

    update flow

--- flow/flow.js
+++ flow/flow.js
@@ -30,3 +30,46 @@ setTimeout(() => {
         console.log(12)
     })
 },2);
+
+
+//node版本不同对执行顺序有不一样的修改
+//https://www.jianshu.com/p/837b584e1bdd
+//http://www.ruanyifeng.com/blog/2018/02/node-event-loop.html
+//下面的代码会按照task队列的形式走，既timer队列和check队列走完，才会切换到microTask
+
+// process.nextTick(function () {
+//     console.log(1)
+// })
+//
+// process.nextTick(function () {
+//     console.log(2)
+// })
+//
+// setTimeout(function () {
+//     console.log(10)
+// },0)
+//
+// setImmediate(function () {
+//     console.log(3)
+//     process.nextTick(function () {
+//         console.log(4)
+//     })
+// })
+//
+// setImmediate(function () {
+//     console.log(5)
+// })
+//
+// setTimeout(function () {
+//     console.log(9)
+// },0)
+//
+// setImmediate(function () {
+//     console.log(6)
+// })
+//
+// setTimeout(function () {
+//     console.log(8)
+// },0)
+//
+// console.log(11)
